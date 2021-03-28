e2a4c4ba59ff86d86a14ea6f9d8c5c4988f91426
Samuel <>
2021-03-28 14:17:15 +0200

    init

---
+++ nuxt.config.js
@@ -0,0 +1,61 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'ecoshop',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    "@/assets/css/all.css",
+    "assets/css/carousel-ticker.css",
+    "assets/css/bootsnav.css",
+    "assets/css/animate.css",
+    "assets/css/code_animate.css",
+    "assets/css/animate.css",
+    "@/assets/css/jquery-ui.css",
+
+
+
+
+    "assets/css/bootstrap.min.css",
+    "assets/css/bootstrap-select.css",
+    "assets/css/style.css",
+    "assets/css/responsive.css",
+    "assets/css/superslides.css",
+    "assets/css/custom.css",
+
+    "assets/css/owl.carousel.min.css",
+
+    // "https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css",
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/template.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<MainTop></MainTop>
+
+		<Header></Header>
+		<!-- Put content here -->
+
+		<InstagramFeed></InstagramFeed>
+		<!-- End Instagram Feed  -->
+
+		<!-- Start Footer  -->
+		<Footer></Footer>
+	</div>
+</template>
+
+<script>
+export default {
+	components: {
+		components: {
+			Header: () => import("@/components/Header"),
+			Footer: () => import("@/components/Footer"),
+			MainTop: () => import("@/components/MainTop"),
+			InstagramFeed: () => import("@/components/InstagramFeed"),
+			TopSearch: () => import("@/components/TopSearch"),
+		},
+		data() {
+			return {};
+		},
+	},
+};
+</script>
